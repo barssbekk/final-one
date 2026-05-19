430210ddc7182289c31a787edf935b3a7496c4d3
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-19 12:07:27 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+    fstream fileInput{};
+    if (!fileInput)
+        cerr << "File not found\n";
+
+    map<string, int> airportTraffic;
 
     return 0;
 }
