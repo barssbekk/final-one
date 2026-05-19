112a8219d29faf27e1758de7fb8844a5c4e55f3d
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-19 12:27:17 -0700

    update

--- main.cpp
+++ main.cpp
@@ -6,15 +6,23 @@
 using namespace std;
 
 int main() {
-    fstream fileInput{};
-    if (!fileInput)
+    fstream fileInput{"data.txt"};
+    if (!fileInput) {
         cerr << "File not found\n";
+        return 1;
+    }
 
     map<string, int> airportTraffic;
     string initial{};
     string final{};
     while (fileInput >> initial >> final) {
-        airportTraffic[initial] =
+        ++airportTraffic[initial];
+        ++airportTraffic[final];
+    }
+
+    cout << "All airport traffic counts: \n";
+    for (auto& each : airportTraffic) {
+        cout << each.first << '\t' << each.second << '\n';
     }
 
     return 0;
